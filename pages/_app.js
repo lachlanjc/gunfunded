ea9d23533fcb412a87841c267ce8622d5ea4ebf8
Lachlan Campbell <lachlan@hackclub.com>
2019-10-09 16:56:36 -0400

    Reinstate Automatic Static Optimization

--- pages/_app.js
+++ pages/_app.js
@@ -9,38 +9,22 @@ import Footer from '../components/footer'
 const name = 'Gun Funded'
 const desc = 'Rescuing democracy'
 
-export default class extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
-  render() {
-    const { Component, pageProps } = this.props
-
-    return (
-      <Container>
-        <Head>
-          <title>{name}</title>
-          <meta property="twitter:card" content="summary" />
-          <meta property="twitter:site" content="@lachlanjc" />
-          <meta property="twitter:description" content={desc} />
-          <meta property="og:title" content={name} />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://gunfunded.now.sh/" />
-          <meta property="description" content={desc} />
-        </Head>
-        <ThemeProvider theme={theme}>
-          <Global styles={theme => ({ body: theme.styles.root })} />
-          <Component {...pageProps} />
-          <Footer />
-        </ThemeProvider>
-      </Container>
-    )
-  }
-}
+export default ({ Component, pageProps }) => (
+  <Container>
+    <Head>
+      <title>{name}</title>
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:site" content="@lachlanjc" />
+      <meta property="twitter:description" content={desc} />
+      <meta property="og:title" content={name} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://gunfunded.now.sh/" />
+      <meta property="description" content={desc} />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <Global styles={theme => ({ body: theme.styles.root })} />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  </Container>
+)
