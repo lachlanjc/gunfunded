337401fd3d766e76d7f66d6abd2c7084ffcd33a6
Glitch (gunfunded) <none>
2019-12-01 18:40:34 +0000

    Group groups by cycle

--- pages/groups.js
+++ pages/groups.js
@@ -1,32 +1,28 @@
 import fetch from 'isomorphic-unfetch'
-import { Container, Box, Badge, Flex, Heading, Card, Text } from '@theme-ui/components'
+import {
+  Container,
+  Box,
+  Badge,
+  Flex,
+  Heading,
+  Card,
+  Text
+} from '@theme-ui/components'
 import Header from '../components/header'
 import Stat, { StatGrid } from '../components/stat'
 import commaNumber from 'comma-number'
-import { capitalize, map, sum } from 'lodash'
+import { capitalize, map, sum, groupBy, reverse } from 'lodash'
 
 const Group = ({ id, pac, cycle, amount, type }) => (
   <Flex
     sx={{
-      borderBottom: '1px solid',
+      borderTop: '1px solid',
       borderColor: 'sunken',
       color: 'text',
       py: 2
     }}
   >
-    <Text
-      sx={{
-        fontSize: 1,
-        fontWeight: 'bold',
-        my: 0,
-        color: 'muted',
-        transform: 'rotate(180deg)',
-        writingMode: 'tb'
-      }}
-    >
-      {cycle}
-    </Text>
-    <Box sx={{ ml: [2, 3], mr: 'auto' }}>
+    <Box sx={{ mr: 'auto' }}>
       <Text sx={{ fontSize: 1, fontWeight: 'bold', my: 0 }}>{pac}</Text>
       <Badge
         variant="pill"
@@ -35,8 +31,7 @@ const Group = ({ id, pac, cycle, amount, type }) => (
           fontSize: 0,
           fontWeight: 'normal',
           bg: type === 'control' ? 'dem' : 'repub',
-            px: 2
-          // textTransform: 'uppercase'
+          px: 2
         }}
       >
         Gun {capitalize(type)}
@@ -46,15 +41,26 @@ const Group = ({ id, pac, cycle, amount, type }) => (
   </Flex>
 )
 
-const Page = ({ groups }) => (
+const Page = ({ cycles }) => (
   <Box as="main" sx={{ bg: 'background' }}>
     <Header
       title="PACs"
       desc="These are the top PACs giving money to Congress on gun issues."
     />
     <Container as="article" sx={{ py: [3, 4] }}>
-      {groups.map(group => (
-        <Group key={group.id} {...group} />
+      {Object.keys(cycles).map(year => (
+        <Box
+          as="section"
+          key={year}
+          sx={{ mb: [3, 4], borderBottom: '1px solid', borderColor: 'sunken' }}
+        >
+          <Heading as="h2" variant="heading" sx={{ color: 'accent', mb: 2 }}>
+            {year}
+          </Heading>
+          {cycles[year].map(group => (
+            <Group key={group.id} {...group} />
+          ))}
+        </Box>
       ))}
     </Container>
   </Box>
@@ -64,12 +70,12 @@ Page.getInitialProps = async ({ req }) => {
   const origin = req ? `http://${req.headers.host}` : ''
   const data = await fetch(`${origin}/api/groups`)
   const groups = await data.json()
-  console.log(groups)
+  const cycles = groupBy(groups, 'cycle')
   // const totals = map(profiles, 'amount')
   // const stats = {
   //   total: sum(totals)
   // }
-  return { groups }
+  return { cycles }
 }
 
 export default Page
