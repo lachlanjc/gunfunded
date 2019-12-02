1ff4895a39c9dc9e06ee6dd9209987aba1d2698f
Glitch (gunfunded) <none>
2019-12-02 05:20:08 +0000

    Remove StateShape

--- pages/state-shape.js
+++
@@ -1,9 +0,0 @@
-import StateShape from '../components/state-shape'
-
-export default () => (
-  <div style={{ background: 'magenta', marginTop: 32 }}>
-    <StateShape id="PA" />
-    <StateShape id="TX" />
-    <StateShape id="CA" />
-  </div>
-)
