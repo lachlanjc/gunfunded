da9f8023fb86c1695a428165a928e425016015e0
Lachlan Campbell <lachlan@hackclub.com>
2019-12-17 04:41:12 -0500

    Add favicons

--- components/meta.js
+++ components/meta.js
@@ -20,5 +20,11 @@ export default ({
     <meta key="twitter_card" name="twitter:card" content="summary_large_image" />
     <meta key="twitter_img" name="twitter:image" content={image} />
     <meta key="theme_color" name="theme-color" content={color} />
+    <meta key="tile_color" name="msapplication-TileColor" content={color} />
+    <link key="safari_icon" rel="mask-icon" href="/safari-pinned-tab.svg" color={color} />
+    <link key="apple_icon" rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link key="favicon_32" rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link key="favicon_16" rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link key="manifest" rel="manifest" href="/site.webmanifest" />
   </Head>
 )
