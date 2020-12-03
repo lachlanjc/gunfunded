9f17ca40516a833ca697502d8ad56c3998b9a3f5
Lachlan Campbell <lachlanjc@hey.com>
2020-12-03 14:46:31 -0500

    Fix footer

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,16 @@
-import Footer from './footer.mdx'
+import Content from './footer.mdx'
 import { Flex, Box } from 'theme-ui'
 
-export default () => (
+const Footer = () => (
   <Box
     as="footer"
-    sx={{ bg: 'sunken', textAlign: 'center', px: 2, py: [3, 4], mt: [4, 5] }}
-  >
-    <Flex
-      sx={{
-        alignItems: 'center',
-        justifyContent: 'center',
+    sx={{ bg: 'sunken', textAlign: 'center', px: 2, py: [3, 4], mt: [4, 5],
         p: { color: 'muted' },
         a: { color: 'accent', ':first-of-type': { fontWeight: 'bold' } }
       }}
     >
-      <Footer />
-    </Flex>
+      <Content />
   </Box>
 )
+
+export default Footer
